c64350d0b1e4bd9ef5b99855924bf60e6e63d558
ravi devineni <ravi.devineni@qount.io>
2018-12-13 23:14:32 +0530

    Added Cassandra properties

---
+++ src/main/java/com/example/demo/cassandra/repository/CustomerRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.cassandra.repository;
+
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.AllowFiltering;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+import com.example.demo.cassandra.model.Customer;
+
+public interface CustomerRepository extends CassandraRepository<Customer, String> {
+	 
+	@AllowFiltering
+	public List<Customer> findByFirstname(String firstname);
+ 
+	@AllowFiltering
+	public List<Customer> findCustomerHasAgeGreaterThan(int age);
+}
