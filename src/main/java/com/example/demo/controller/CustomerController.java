ae887246e88cd9a56b7c88f5df7a88f98bcda582
ravi devineni <ravi.devineni@qount.io>
2018-12-16 15:45:23 +0530

    rest end points are working good now.

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -3,43 +3,45 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.cassandra.model.Customer;
 import com.example.demo.cassandra.model.CustomerRequest;
 import com.example.demo.service.CustomerService;
 
-@Controller
-@RequestMapping(path = "customers")
+@RestController
+@RequestMapping(value = "api/customers")
 public class CustomerController {
 	
 	@Autowired
 	private CustomerService customerService;
 
-	@RequestMapping(path = "", method = RequestMethod.POST, produces = { "application/json" })
+	@PostMapping
 	public Customer add(@RequestBody CustomerRequest customerRequest) {
 		return customerService.save(customerRequest);
 
 	}
 
-	@RequestMapping(path = "{customerId}", method = RequestMethod.PUT, produces = { "application/json" })
+	@PutMapping("/{customerId}")
 	public Customer update(@PathVariable(name = "customerId") int customerId, @RequestBody CustomerRequest customerRequest) {
 		customerRequest.setId(customerId);
 		return customerService.update(customerRequest);
 
 	}
 
-	@RequestMapping(path = "{customerId}", method = RequestMethod.GET, produces = { "application/json" })
+	@GetMapping("/{customerId}")
 	public Customer get(@PathVariable(name = "customerId") int customerId) {
 		return customerService.get(customerId);
 
 	}
 
-	@RequestMapping(path = "", method = RequestMethod.GET, produces = { "application/json" })
+	@GetMapping
 	public List<Customer> getAll() {
 		return customerService.getAll();
 
